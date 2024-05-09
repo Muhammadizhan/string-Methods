d2ddddda99ad7a10281c87639b12427c06bdbcdb
Muhammadizhan <izhanayaz4@gmail.com>
2024-05-09 19:58:08 +0500

    4 question solve

---
+++ app.js
@@ -0,0 +1,17 @@
+// Question - 01
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var fullName = firstName + " " + lastName;
+alert("Hello, " + fullName + "! Welcome.");
+
+// question - 02
+var favoriteModel = prompt("What is your favorite mobile phone model?");
+var length = favoriteModel.length;
+alert("The length of your favorite mobile phone model is: " + length);
+
+// Question - 03
+var word = "Pakistani";
+var index = word.indexOf("n");
+alert("The index of 'n' in the word 'Pakistani' is: " + index);
+
+// Question - 04
